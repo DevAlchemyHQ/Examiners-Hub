6aaa6be9e860972d340c285fa5eca37881849f4b
Calvin <munenecalvn@gmail.com>
2025-02-20 17:14:29 +0300

    Remove unused imports from home component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { useNavigate, useParams } from 'react-router-dom';
 import { Loader2 } from 'lucide-react';
 import { useMetadataStore } from '../store/metadataStore';
 import { useProjectStore } from '../store/projectStore';
